92c6ec262c8eb2852afabeff6ea4cef38a8cee39
SWWWin <bgpd1258@naver.com>
2024-12-30 16:09:54 +0900

    first commit

---
+++ q17427.java
@@ -0,0 +1,13 @@
+package math;
+
+import java.util.Scanner;
+public class q17427 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int answer = 0;
+        for(int i = 1; i < a; i ++) {
+            if()
+        }
+    }
+}
