f614f6b8a66e8d46a180973e30fce063b4380d47
SWWWin <bgpd1258@naver.com>
2025-04-29 23:55:39 +0900

    feat: dfs 예제 문제

---
+++ Backjoon/q1697.java
@@ -0,0 +1,49 @@
+package math.Backjoon;
+
+import java.util.*;
+
+public class q1697 {
+    static final int MAX = 100001;  // 0부터 100,000까지
+    static boolean[] visited = new boolean[MAX];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();  // 수빈이 위치
+        int k = sc.nextInt();  // 동생 위치
+
+        System.out.println(bfs(n, k));
+    }
+
+    static int bfs(int start, int target) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(start, 0));
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+
+            if (current.pos == target) {
+                return current.time;
+            }
+
+            int[] nextPositions = {current.pos - 1, current.pos + 1, current.pos * 2};
+            for (int next : nextPositions) {
+                if (next >= 0 && next < MAX && !visited[next]) {
+                    visited[next] = true;
+                    queue.add(new Node(next, current.time + 1));
+                }
+            }
+        }
+        return -1;  // 절대 도달할 수 없는 경우는 없지만, 형식상 넣음
+    }
+
+    static class Node {
+        int pos;
+        int time;
+
+        Node(int pos, int time) {
+            this.pos = pos;
+            this.time = time;
+        }
+    }
+}
